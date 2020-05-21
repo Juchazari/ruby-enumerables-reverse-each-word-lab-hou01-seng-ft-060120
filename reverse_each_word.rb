ac51de450dfe6d37b13518c92237e168912660f8
Julio Chazari <julio.chazari@gmail.com>
2020-05-21 05:04:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,15 @@ def reverse_each_word(string)
   end
   reversed_words_array.join(" ")
 end
+
+def reverse_each_word(string)
+  
+  string_to_array = string.split
+  reversed_words_array = []
+  
+  string_to_array.each do |i|
+    reversed_words_array << i.reverse
+  end
+  reversed_words_array.join(" ")
+  
+end
